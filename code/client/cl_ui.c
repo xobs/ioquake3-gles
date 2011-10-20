422b44f50f36e543d985198e8b6656b4f58064e6
Fathi Boudra <fabo@debian.org>
2011-10-20 15:46:50 +0300

    work around Bad UI System Trap 53

--- code/client/cl_ui.c
+++ code/client/cl_ui.c
@@ -1060,7 +1060,8 @@ intptr_t CL_UISystemCalls( intptr_t *args ) {
 
 		
 	default:
-		Com_Error( ERR_DROP, "Bad UI system trap: %ld", (long int) args[0] );
+		//Com_Error( ERR_DROP, "Bad UI system trap: %ld", (long int) args[0] );
+		break;
 
 	}
 
